558fec98a06da991b7b387af69e66921f950bc98
nlw0 <nwerneck@gmail.com>
2015-09-16 11:32:30 -0300

    the queens refuse to capitulate

--- src/main/scala/hackerrank/queensonboard/Solution.scala
+++ src/main/scala/hackerrank/queensonboard/Solution.scala
@@ -2,16 +2,6 @@ package hackerrank.queensonboard
 
 object Solution {
 
-  sealed trait Node {
-    def dom: collection.SortedMap[Int, Set[Int]]
-
-    def inc: Int
-  }
-
-  case class NodeDo(dom: collection.SortedMap[Int, Set[Int]], inc: Int) extends Node
-
-  case class NodeMemo(dom: collection.SortedMap[Int, Set[Int]], inc: Int) extends Node
-
   def main(args: Array[String]): Unit = {
     val is = sys.props.get("local") map io.Source.fromFile getOrElse io.Source.fromInputStream(System.in)
     val input: Iterator[String] = is.getLines()
@@ -20,49 +10,35 @@ object Solution {
 
     def boards = for (i <- (1 to t).iterator) yield readBoard(input)
 
-    def result = boards map { bb =>
-      if (bb.isEmpty) 0 else solve(List(NodeDo(bb, bb.firstKey)))
-    }
+    def result = boards map (bb => solve(bb))
 
     result foreach println
   }
 
-//  val memo = collection.mutable.HashMap[collection.SortedMap[Int, Set[Int]], Int]()
+  val memo = collection.mutable.HashMap[Map[Int, Set[Int]], Int]()
 
-  val memoInit = Map[Node, Int](
-    NodeDo(collection.SortedMap(), 0) -> 0,
-    NodeDo(collection.SortedMap(), 1) -> 1)
+  def solve(dom: Map[Int, Set[Int]]): Int = {
+    if (memo contains dom) memo(dom)
+    else if (dom.isEmpty) 0
+    else {
+      val pos = dom.keys.min
 
-  @annotation.tailrec
-  def solve(stack: List[Node], acc: Int = 0, memo: Map[Node, Int] = memoInit): Int =
-    stack match {
-      case Nil => acc
+      val aa = if (dom contains pos) 1 + solve(dom.filterKeys(_ >= pos + 1) -- dom(pos)) else 0
+      val bb = solve(dom.filterKeys(_ >= pos + 1))
 
-      case nn :: st if memo contains nn =>
-//        println("yoy")
-        solve(st, acc + memo(nn), memo)
-
-      case NodeDo(dom, inc) :: st =>
-        val pos = dom.firstKey
-        val na = NodeDo(dom.from(pos + 1), 0)
-        val nb = NodeDo(na.dom -- dom(pos), 1)
-        val newStack = na :: nb :: NodeMemo(dom, acc) :: st
-        solve(newStack, acc + inc, memo)
-
-      case NodeMemo(dom, inc) :: st =>
-//        println("record" + dom + " -> " + inc)
-        solve(st, acc, memo + (NodeDo(dom, inc) -> (acc - inc)))
+      val rr = (aa + bb) % 1000000007
+      memo(dom) = rr
+      rr
     }
+  }
 
   def readBoard(input: Iterator[String]) = {
     val Array(n, m) = input.next().split(" ").take(2).map(_.toInt)
     val chr = for (i <- 1 to n) yield input.next().take(m)
 
-    def blocks = for {
-      i <- chr.indices
-      j <- chr.head.indices
-      if chr(i)(j) == '#'
-    } yield i * (m + 1) + j
+    def blocks = for {i <- chr.indices
+                      j <- chr.head.indices
+                      if chr(i)(j) == '#'} yield i * (m + 1) + j
 
     def domain = for {
       i <- 0 until n
@@ -71,13 +47,13 @@ object Solution {
       pos = i * (m + 1) + j
     } yield pos -> genDomain(blocks.toSet, n, m, pos)
 
-    collection.SortedMap[Int, Set[Int]] (domain: _*)
+    Map[Int, Set[Int]](domain:_*)
   }
 
   def genDomain(blocks: Set[Int], n: Int, m: Int, pos: Int) = {
     val k = m + 1
     def dirs = Array(1, k - 1, k, k + 1)
-    dirs.iterator.foldLeft(Set[Int] ()) {
+    dirs.iterator.foldLeft(Set[Int]()) {
       (dom, dir) => dom ++ genDomDirection(blocks, n, m, pos + dir, dir)
     }
   }
@@ -92,6 +68,7 @@ object Solution {
                        ): Set[Int] = {
     def invalid(x: Int) = blocks.contains(x) || !bounds(x)
 
+    // def bounds(x: Int) = x >= 0 && x % (n + 1) < n && x < m * (n + 1)
     def bounds(x: Int) = x >= 0 && x % (m + 1) < m && x < n * (m + 1)
 
     if (invalid(pos)) acc
